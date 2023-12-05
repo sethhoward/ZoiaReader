29ca40a8c67a6ff7fdf92ae3b925fc2db744b124
Seth Howard <spatialreasoning@gmail.com>
2023-12-05 14:26:07 -0700

    Initial Commit

---
+++ ZoiaReader/ZoiaReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZoiaReaderApp.swift
+//  ZoiaReader
+//
+//  Created by Seth Howard on 12/5/23.
+//
+
+import SwiftUI
+
+@main
+struct ZoiaReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
